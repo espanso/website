1809fbd1d089f512ccf3b727be00eb93bb4a325b
Auca Maillot <aucacoyan@gmail.com>
2025-09-13 12:47:25 -0300

    Revert 'Update docusaurus.config.js'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const CURRENT_STABLE_VERSION = "v2.2.3";
+const CURRENT_STABLE_VERSION = "v2.2.1";
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
